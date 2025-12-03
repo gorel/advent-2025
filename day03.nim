859437ee40d09180212d206dc6945d6c0f9fb629
Logan Gore <lgore93@gmail.com>
2025-12-02 21:21:09 -0800

    Day 03

--- day03.nim
+++ day03.nim
@@ -1,16 +1,41 @@
 import strutils, sequtils
 
 proc parseLine(line: string): seq[int] =
-  line.split(",").mapIt(it.parseInt())
+  line.mapIt(ord(it) - ord('0'))
 
-proc solve1(data: seq[seq[int]]): int = 0
+proc sub(line: seq[int], leftMin: int, remaining: int): tuple[n: int, idx: int] =
+  var hi = 0
+  var hiIdx = -1
+  for i in leftMin ..< line.len - remaining:
+    if line[i] > hi:
+      hi = line[i]
+      hiIdx = i
+  return (hi, hiIdx)
 
-proc solve2(data: seq[seq[int]]): int = 0
+proc solve1(g: seq[seq[int]]): int =
+  var res = 0
+  for line in g:
+    let sub0 = sub(line, 0, 1)
+    let sub1 = sub(line, sub0.idx + 1, 0)
+    res += 10*sub0.n + sub1.n
+  return res
+
+proc solve2(g: seq[seq[int]]): int =
+  var res = 0
+  for line in g:
+    var cur = 0
+    var curIdx = -1
+    for i in 0 ..< 12:
+      let subi = sub(line, curIdx + 1, 11 - i)
+      cur = cur*10 + subi.n
+      curIdx = subi.idx
+    res += cur
+  return res
 
 when isMainModule:
   let input = readAll(stdin).strip()
   let lines = input.splitLines()
-  let data = lines.map(parseLine)
+  let g = lines.map(parseLine)
 
-  echo solve1(data)
-  echo solve2(data)
+  echo solve1(g)
+  echo solve2(g)
