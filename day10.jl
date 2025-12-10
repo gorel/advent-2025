802858fa25678fc7d6ea8d3e93b1ee49868858c4
Logan Gore <lgore93@gmail.com>
2025-12-10 08:56:42 -0800

    More prep

--- day10.jl
+++ day10.jl
@@ -1,10 +1,21 @@
+struct Point
+  x :: Int64
+  y :: Int64
+end
+
 function main()
   lines = String[]
+  nums = Point[]
   for line in eachline(stdin)
+    parts = collect(eachsplit(line, ","))
+    x = parse(Int64, parts[1])
+    y = parse(Int64, parts[2])
     println("Line: ", line)
     push!(lines, line)
+    push!(nums, Point(x, y))
   end
   println("Lines: ", lines)
+  println("Points: ", nums)
 end
 
 main()
